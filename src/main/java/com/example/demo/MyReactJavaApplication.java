f57af180f4e3f2209980123651d8286c1c8004ea
mikeprikhodko <mprykhodko@playq.net>
2019-03-18 16:21:46 +0200

    Initial commit

---
+++ src/main/java/com/example/demo/MyReactJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyReactJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyReactJavaApplication.class, args);
+	}
+
+}
